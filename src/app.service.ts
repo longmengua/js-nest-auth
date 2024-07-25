fc34dd6df79da27e16840b44a7691ffab336a70f
Waltor <longmengua@gmail.com>
2024-07-26 04:19:40 +0800

    feat: load env

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { version } from '../package.json';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getVersion() {
+    return {
+      version: version,
+    };
   }
 }
